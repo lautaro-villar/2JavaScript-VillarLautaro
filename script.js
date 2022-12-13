010aa2a0fafc5ecb81256a745f73bee9652728f6
villar lautaro <lautaromartinvillar@gmail.com>
2022-12-12 22:26:54 -0300

    trabajo 2

--- script.js
+++ script.js
@@ -1,31 +1,44 @@
  alert("Bienvenido a mi fixture del mundial!")
 let opciones = prompt(" ingrese 1 para ver las fases de grupos \n ingrese 2 para ver la actualidad de los partidos") 
 
-function grupo( Grupo, EquipoClasificado, PuntosEquipoClasificado ) {
+function /*grupo*/ Equipos( letra, Grupo, EquipoClasificado, PuntosEquipoClasificado ) {
 
-this.letra  = letra  
+this.letra  = letra
 this.Grupo = Grupo
 this.EquipoClasificado = EquipoClasificado
 this.PuntosEquipoClasificado = PuntosEquipoClasificado
 
 }
-const grupo = [
- /*const grupo*/ /*= /*[*/new equipos1("A" , " 1) paises bajos \n 2) senegal \n 3) ecuador \n 4) qatar \n " , " Paises Bajos \n  senegal \n " , "7 \n 6 \n "  ) ]
- /*const grupo*/ /*= /*[*/new equipos2("B" , " 1) Inglaterra   \n 2) Estados Unidos \n 3) Iran \n 4) gales \n " , "inglaterra \n EstadosUnidos \n " , "7 \n 5 \n "  ) 
- /*const grupo*/ /*= /*[*/new equipos3("C" , " 1) argentina \n 2) polonia \n 3) mexico  \n 4) arabia saudita \n  " , " argentina \n polonia   " ,  "6 \n 4 \n "  ) 
- /*const grupo*/ /*= /*[*/new equipos4("E" , " 1) japon  \n 2) españa  \n 3) alemania  \n 4) costa rica  \n " , " japon \n españa " ,  "6 \n 4 \n "  ) 
- /*const grupo*/ /*= /*[*/new equipos5("F" , " 1) marruecos \n 2) croacia \n 3) belgica \n 4) canada \n " , " marruecos \n croacia " ,  "7 \n 5 \n "  ) 
- /*const grupo*/ /*= /*[*/new equipos6("G" , " 1) brasil \n 2) suiza  \n 3) camerun \n 4) serbia \n " , " brasil \n suiza " ,  "6 \n 6 \n "  ) 
- /*const grupo*/ /*= /*[*/new equipos7("H" , " 1) portugal \n 2) corea del sur \n 3) uruguay \n 4) ghana \n " , " portugal \n corea del sur " ,  "6 \n 4 \n "  ) 
 
 
-/*let primeraOpcion = "1"
 
-if (opciones == primeraOpcion) {
+const Grupo = [
+ /*const grupo*/ /*= /*[*/new Equipos("A" , " 1) paises bajos \n 2) senegal \n 3) ecuador \n 4) qatar \n " , " Paises Bajos \n  senegal \n " , "7 \n 6 \n "  )  ,
+ /*const grupo*/ /*= /*[*/new Equipos("B" , " 1) Inglaterra   \n 2) Estados Unidos \n 3) Iran \n 4) gales \n " , "inglaterra \n EstadosUnidos \n " , "7 \n 5 \n "  ) ,
+ /*const grupo*/ /*= /*[*/new Equipos("C" , " 1) argentina \n 2) polonia \n 3) mexico  \n 4) arabia saudita \n  " , " argentina \n polonia   " ,  "6 \n 4 \n "  ),
+ /*const grupo*/ /*= /*[*/new Equipos("E" , " 1) japon  \n 2) españa  \n 3) alemania  \n 4) costa rica  \n " , " japon \n españa " ,  "6 \n 4 \n "  ) ,
+ /*const grupo*/ /*= /*[*/new Equipos("F" , " 1) marruecos \n 2) croacia \n 3) belgica \n 4) canada \n " , " marruecos \n croacia " ,  "7 \n 5 \n "  ) ,
+ /*const grupo*/ /*= /*[*/new Equipos("G" , " 1) brasil \n 2) suiza  \n 3) camerun \n 4) serbia \n " , " brasil \n suiza " ,  "6 \n 6 \n "  ) ,
+ /*const grupo*/ /*= /*[*/new Equipos("H" , " 1) portugal \n 2) corea del sur \n 3) uruguay \n 4) ghana \n " , " portugal \n corea del sur " ,  "6 \n 4 \n "  ) ,
+]
+let primeraOpcion = "1"
+
+ if (opciones == primeraOpcion) {
+    let equipos = prompt("Ingrese un Grupo:\n A \n B \n C \n D \n E \n F \n G \n H \n  ")
+    /*let LetraElegida = prompt("Elige la informacion que quieres tener sobre el grupo " + faseDeGrupos + "\n \n •equipos \n \n •equipo clasificado \n \n •puntos de los equipos clasificados")*/
+  }
+
+
+
+
+
+
+
+/*if (opciones == primeraOpcion) {
   let faseDeGrupos = prompt("Ingrese un Grupo:\n A \n B \n C \n D \n E \n F \n G \n H \n  ".toLocaleUpperCase) 
   let LetraElegida = prompt("Elige la informacion que quieres tener sobre el grupo " + faseDeGrupos + "\n \n •equipos \n \n •equipo clasificado \n \n •puntos de los equipos clasificados")
+ }*/
 
-}*/
 
 
  
